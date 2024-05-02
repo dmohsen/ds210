38ed3cb41685fbacfc74fd47cc5b429c9a0ab2af
dmohsen <dmohsen@bu.edu>
2024-05-02 17:50:40 -0400

    created basic functions for adding vertexes and edges to a graph as well as creating the graph struct

--- final project/spotify_songs_graph_analysis/src/main.rs
+++ final project/spotify_songs_graph_analysis/src/main.rs
@@ -2,10 +2,11 @@ use serde::Deserialize;
 use csv::ReaderBuilder;
 use std::fs::File;
 use std::error::Error;
+use std::collections::HashMap;
 
 #[derive(Debug, Deserialize, Clone)]
 struct Song {
-    position: u32,
+    position: usize,
     artist_name: String,
     song_name: String,
     days: u32,
@@ -16,7 +17,35 @@ struct Song {
     total_streams: u64, 
 }
 
+type Vertex = usize;
+type ListOfEdges = Vec<(Vertex, Vertex)>;
+type AdjacencyLists = Vec<Vec<Vertex>>;
 
+struct Graph {
+    vertices: Vec<Song>, 
+    adjacency_list: AdjacencyLists, 
+}
+
+impl Graph {
+    fn new() -> Self {
+        Graph {
+            vertices: vec![],
+            adjacency_list: vec![],
+        }
+    }
+
+    fn add_vertex(&mut self, song: Song) -> usize {
+        let index = self.vertices.len();
+        self.vertices.push(song);
+        self.adjacency_list.push(vec![]);
+        index
+    }
+
+    fn add_edge(&mut self, src: usize, dest: usize) {
+        self.adjacency_list[src].push(dest);
+        self.adjacency_list[dest].push(src); 
+    }
+}
 
 fn load_songs_from_csv(file_path: &str) -> Result<Vec<Song>, Box<dyn Error>> {
     let file = File::open(file_path)?;
@@ -33,14 +62,21 @@ fn load_songs_from_csv(file_path: &str) -> Result<Vec<Song>, Box<dyn Error>> {
 
 
 fn main() {
-    match load_songs_from_csv("Spotify_final_dataset.csv") {
+    let songs = match load_songs_from_csv("Spotify_final_dataset.csv") {
         Ok(songs) => {
             println!("Loaded {} songs.", songs.len());
             for song in songs.iter().take(10) {
                 println!("{:?}", song);
             }
+            songs  
         },
-        Err(e) => eprintln!("Error loading songs: {}", e),
-    }
+        Err(e) => {
+            eprintln!("Error loading songs: {}", e);
+            return; 
+        },
+    };
+
+    let mut graph = Graph::build_from_songs(songs);
 }
+
 
