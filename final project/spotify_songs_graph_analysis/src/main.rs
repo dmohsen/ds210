3158a39094542b5d2f32bc6ef5d4f6cb0b106a66
dmohsen <dmohsen@bu.edu>
2024-05-03 18:35:07 -0400

    added djikstra and BFS and also made a determination to determine edge by differnece in popularity position in songs between a shared artist

--- final project/spotify_songs_graph_analysis/src/main.rs
+++ final project/spotify_songs_graph_analysis/src/main.rs
@@ -2,8 +2,8 @@ use serde::Deserialize;
 use csv::ReaderBuilder;
 use std::fs::File;
 use std::error::Error;
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::{HashMap, VecDeque, BinaryHeap};
+use std::cmp::Reverse;
 
 #[derive(Debug, Deserialize, Clone)]
 struct Song {
@@ -19,8 +19,8 @@ struct Song {
 }
 
 type Vertex = usize;
-type ListOfEdges = Vec<(Vertex, Vertex)>;
-type AdjacencyLists = Vec<Vec<Vertex>>;
+type Weight = isize; // Using isize for weights to utilize integer comparisons
+type AdjacencyLists = Vec<Vec<(Vertex, Weight)>>;
 
 struct Graph {
     vertices: Vec<Song>, 
@@ -31,21 +31,25 @@ impl Graph {
     fn new() -> Self {
         Graph {
             vertices: vec![],
-            adjacency_list: vec![],
+            adjacency_list: vec![], 
         }
     }
 
     fn add_vertex(&mut self, song: Song) -> usize {
         let index = self.vertices.len();
         self.vertices.push(song);
-        self.adjacency_list.push(vec![]);
+        self.adjacency_list.push(Vec::new());
         index
     }
 
-    fn add_edge(&mut self, src: usize, dest: usize) {
-        if !self.adjacency_list[src].contains(&dest) {
-            self.adjacency_list[src].push(dest);
-            self.adjacency_list[dest].push(src); 
+    fn add_weighted_edge(&mut self, src: usize, dest: usize) {
+        let src_pos = self.vertices[src].position;
+        let dest_pos = self.vertices[dest].position;
+        let weight = ((src_pos as isize - dest_pos as isize).abs() * 1000) as isize; 
+
+        if src != dest {
+            self.adjacency_list[src].push((dest, weight));
+            self.adjacency_list[dest].push((src, weight));
         }
     }
     
@@ -53,16 +57,16 @@ impl Graph {
         let mut graph = Self::new();
         let mut artist_map: HashMap<String, Vec<usize>> = HashMap::new();
 
-        for song in songs.into_iter() {
+        for song in songs {
             let index = graph.add_vertex(song.clone());
-            artist_map.entry(song.artist_name).or_default().push(index);
+            artist_map.entry(song.artist_name.clone()).or_default().push(index);
         }
 
         for indices in artist_map.values() {
             for &i in indices {
                 for &j in indices {
                     if i != j {
-                        graph.add_edge(i, j);
+                        graph.add_weighted_edge(i, j);
                     }
                 }
             }
@@ -70,31 +74,61 @@ impl Graph {
 
         graph
     }
-    
-    fn print_graph(&self) {
-        let mut artist_map: HashMap<&String, HashSet<&String>> = HashMap::new();
 
+    fn print_graph(&self) {
         for (i, edges) in self.adjacency_list.iter().enumerate() {
             let artist_name = &self.vertices[i].artist_name;
-            if !edges.is_empty() { 
-                artist_map.entry(artist_name).or_default();
-                for &index in edges {
-                    let connected_song = &self.vertices[index].song_name;
-                    let connected_artist = &self.vertices[index].artist_name;
-                    if artist_name == connected_artist {
-                        artist_map.get_mut(artist_name).unwrap().insert(connected_song);
-                    }
+            println!("{}: {} Connections", artist_name, edges.len());
+            for &(j, weight) in edges {
+                let connected_song = &self.vertices[j].song_name;
+                println!("  - Connects to {} with weight {:.3}", connected_song, weight as f32 / 1000.0);
+            }
+        }
+    }
+
+    fn bfs(&self, start_vertex: usize) -> Vec<usize> {
+        let mut queue = VecDeque::new();
+        let mut visited = vec![false; self.vertices.len()];
+        let mut result = Vec::new();
+
+        visited[start_vertex] = true;
+        queue.push_back(start_vertex);
+
+        while let Some(current) = queue.pop_front() {
+            result.push(current);
+            for &(neighbor, _) in &self.adjacency_list[current] {
+                if !visited[neighbor] {
+                    visited[neighbor] = true;
+                    queue.push_back(neighbor);
                 }
             }
         }
 
-        for (artist, songs) in artist_map {
-            println!("{}: {} Songs", artist, songs.len());
-            for song in songs {
-                println!("  - {} ", song);
+        result
+    }
+
+    fn dijkstra(&self, start_vertex: usize) -> Vec<f32> {
+        let mut distances = vec![f32::MAX; self.vertices.len()];
+        let mut heap = BinaryHeap::new();
+
+        distances[start_vertex] = 0.0;
+        heap.push(Reverse((0, start_vertex)));
+
+        while let Some(Reverse((current_distance, u))) = heap.pop() {
+            if (current_distance as f32) > distances[u] {
+                continue;
             }
-            println!(); 
+
+            for &(v, weight) in &self.adjacency_list[u] {
+                let distance = current_distance + weight;
+                if (distance as f32) < distances[v] {
+                    distances[v] = distance as f32;
+                    heap.push(Reverse((distance, v)));
+                }
+            }
         }
+
+        distances.iter().map(|&d| d / 1000.0).collect()
     }
 }
 
@@ -112,21 +146,7 @@ fn load_songs_from_csv(file_path: &str) -> Result<Vec<Song>, Box<dyn Error>> {
 }
 
 fn main() {
-    let songs = match load_songs_from_csv("Spotify_final_dataset.csv") {
-        Ok(songs) => {
-            println!("Loaded {} songs.", songs.len());
-            for song in songs.iter().take(10) {
-                println!("{:?}", song);
-            }
-            songs  
-        },
-        Err(e) => {
-            eprintln!("Error loading songs: {}", e);
-            return; 
-        },
-    };
-
-    let mut graph = Graph::build_from_songs(songs);
+    let songs = load_songs_from_csv("Spotify_final_dataset.csv").expect("Failed to load songs");
+    let graph = Graph::build_from_songs(songs);
     graph.print_graph();
 }
-
